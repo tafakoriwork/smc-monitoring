05ee641de7114a7088bdc5f636b87bc5ee0fbae0
tafakoriwork <tafakoriwork@gmail>
2022-06-13 18:06:27 +0430

    last

--- src/components/monitoring/CPU/cpuChart.jsx
+++ src/components/monitoring/CPU/cpuChart.jsx
@@ -3,29 +3,27 @@ import { useEffect, useState } from "react";
 import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
 function VChart() {
-  const [datad, setData] = useState([]);
+  const [datad, setData] = useState([
+    {x: 0, y:0}
+  ]);
   let i = 0;
 
-  const smcRequest = async() => {
-    try {
-      const response = await fetch('https://172.30.5.125/smc-sl/api/v1/oshw/cpu/0',{
-      headers:{
-        "Username": "morsa",
-        "Password": "p@ss@ceph",
-        "Address": "172.30.6.45",
-        "Authorization": "Bearer mTiEMSctlXyavwqZ4QbPdVsYp6CrJB9GUAkogfuKzHDW28hn1xL7ROIeNF3j5",
-    }});
-      const result = await response;
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-   
+  const smcRequest = () => {
+    fetch('http://172.30.5.131/smc-monitoringlaravel/public/')
+    .then(function(response) {
+          return response.json();   
+  
+        }).then(data => {
+        if(data) {
+        const newval = {x: Math.floor(data['cpu usage']), y: Math.floor(data['cpu current speed'])}
+        setData(datad=> [...datad, newval]);
+      }
+      })
   };
-
-  useEffect(() => {
-    smcRequest();
-  }, []);
+  
+setInterval(() => {
+  smcRequest();
+}, 4000);
   return (
     <VictoryChart theme={VictoryTheme.material}>
       <VictoryLine
